b64a24377376238c8d8e4e89e99d2fcb2fc9b470
yulimereles <yuuumereles@gmail.com>
2023-10-09 12:18:05 -0300

    conexion a bases de datos exitosa

--- database.js
+++ database.js
@@ -1,7 +1,7 @@
 import { connect } from "mongoose";
 
 export function connectMongo () {
-    connect(process.env.MONGO.URI)
+    connect(process.env.MONGO_URI)
     //connet* devuelve una promesa y se utiliza .then
     // dentro de una funcion muestra un mensaje que se conecto y muestra el nombre databaseName
     .then(db => console.log(`DB conectada exitoso${db.connection.db.databaseName}`))
